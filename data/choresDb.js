84801f0a2be472cb688c17626b51992e284dec67
Katie Rose <katiefelten@gmail.com>
2019-08-09 15:24:58 -0400

    :ambulance: removed unnecessary files/routing.. simplified further

---
+++ data/choresDb.js
@@ -0,0 +1,25 @@
+const chores = [
+  {
+    choresId: 1,
+    description: "Take Kilo outside",
+    notes: "Fat boy",
+    assignedTo: 1,
+    completed: false
+  },
+  {
+    choresId: 2,
+    description: "Do the dishes",
+    notes: "It's a mess",
+    assignedTo: 1,
+    completed: true
+  },
+  {
+    choresId: 3,
+    description: "Go to the gym",
+    notes: "Get huge",
+    assignedTo: 2,
+    completed: false
+  }
+];
+
+module.exports = chores;
--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require("express");
-
 const server = express();
-const userRouter = require("./users/userRouter");
-const serverRouter = require("./server");
 
-server.use(express.json());
+const userDb = require("./data/userDb");
+const choreDb = require("./data/choresDb");
 
-server.use("/", serverRouter);
-server.use("/api/users", userRouter);
+router.get("/", (req, res) => {
+  res.status(200).json(userDb);
+});
+
+router.get("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  let [currentPerson] = userDb.filter(person => person.userId == userId);
+  if (currentPerson) {
+    res.status(200).json(currentPerson);
+  } else {
+    res.status(404).json({ message: "Unable to find that user" });
+  }
+});
+
+router.get("/:userId/chores/:choresId", (req, res) => {
+  const choresId = req.params.choresId;
+  const completed = req.query.completed;
+
+  let [currentChore] = choreDb.filter(chores => chores.choresId == choresId);
+
+  if (currentChore) {
+    if (completed === "true") {
+      res.status(200).json({ message: "This user has completed their task" });
+    }
+  } else {
+    res.status(400).json({ message: "This user is lazy" });
+  }
+});
 
 server.listen(8000, () => {
   console.log("server running on 8000");
