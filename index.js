62cfec1b3aaf8efd39b044158eebcb83ca023c21
Katie Rose <katiefelten@gmail.com>
2019-08-10 13:32:07 -0400

    :rocket: updated GET completed chores and added GET chores by id

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ server.get("/", (req, res) => {
 });
 
 server.get("/chores", (req, res) => {
-  const filteredArr = choreDb.filter(chore => {
+  const completedChores = choreDb.filter(chore => {
     return chore.completed.toString() === req.query.completed;
   });
-  res.status(200).json(filteredArr);
+  res.status(200).json(completedChores);
 });
+
+server.get("/chores/:id", (req, res) => {
+const id = req.params.id;
+let (currentChore) = choreDb.filter(chore => chore.assignedTo == id);
+if (currentChore) {
+    res.status(200).json(currentChore);
+} else {
+    res.status(404).json([]);
+}
+})
 
 const port = 8000;
 server.listen(port, () => console.log("server is running"));
