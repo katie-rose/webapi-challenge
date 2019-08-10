f1028761042e754f9f70566c47f36013bda9fed5
Katie Rose <katiefelten@gmail.com>
2019-08-10 13:39:54 -0400

    :rocket: fixed GET chores by id, added POST chores

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
-const choreDb = require("./data/choreDb");
+let choreDb = require("./data/choreDb");
 const userDb = require("./data/userDb");
 
 server.get("/", (req, res) => {
@@ -18,12 +18,19 @@ server.get("/chores", (req, res) => {
 
 server.get("/chores/:id", (req, res) => {
 const id = req.params.id;
-let (currentChore) = choreDb.filter(chore => chore.assignedTo == id);
+let [currentChore] = choreDb.filter(chore => chore.assignedTo == id);
 if (currentChore) {
     res.status(200).json(currentChore);
 } else {
     res.status(404).json([]);
 }
+})
+
+server.post("/chores", (req, res) => {
+    const newChore = req.body
+
+chore.Db.push(newChore);
+res.status(200).json(choreDb);
 })
 
 const port = 8000;
