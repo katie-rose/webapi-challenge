e952674f0bf3a87fba10cd09e2515bbe4a63bb4b
Katie Rose <katiefelten@gmail.com>
2019-08-10 13:13:01 -0400

    :rocket: created user array, chores array, basic index setup

---
+++ data/userDb.js
@@ -0,0 +1,16 @@
+const users = [
+  { 
+      id: 1, 
+      name: "Katie"
+  },
+  {
+      id: 2, 
+      name: "Chris"
+  }, 
+  {
+      id: 3, 
+      name: "Kilo"
+  }
+];
+
+module.exports = users;
---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+const port = 8000; 
+server.listen(port, () => console.log("server is running"));
