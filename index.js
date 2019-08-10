ebb11a420ee67cebc47c0b2d211143cbaf2bcc82
Katie Rose <katiefelten@gmail.com>
2019-08-10 13:24:16 -0400

    :rocket: GET filtered array complete

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 server.use(express.json());
 
-const port = 8000; 
+const choreDb = require("./data/choreDb");
+const userDb = require("./data/userDb");
+
+server.get("/", (req, res) => {
+  res.status(200).json(userDb);
+});
+
+server.get("/chores", (req, res) => {
+  const filteredArr = choreDb.filter(chore => {
+    return chore.completed.toString() === req.query.completed;
+  });
+  res.status(200).json(filteredArr);
+});
+
+const port = 8000;
 server.listen(port, () => console.log("server is running"));
